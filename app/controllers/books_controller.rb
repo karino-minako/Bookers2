b639034aa8b28e0870d8b818c12cc8c476f4f27d
karino-minako <irikiiomo11@gmail.com>
2020-04-25 21:15:43 +0900

    [Finish] 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,9 +12,14 @@ class BooksController < ApplicationController
   end
 
   def create
-      book = Book.new(book_params)
-      book.save
-      redirect_to book_path(book.id)
+      @book = Book.new(book_params)
+      if @book.save
+         flash[:notice] = 'you have created book successfully'
+        redirect_to book_path(@book)
+      else
+        @books = Book.all
+        render action: :index
+      end
   end
 
   def edit
@@ -22,14 +27,19 @@ class BooksController < ApplicationController
   end
 
   def update
-      book = Book.find(params[:id])
-      book.update(book_params)
-      redirect_to book_path(book)
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+         flash[:notice] = 'you have created book successfully'
+        redirect_to book_path(@book)
+      else
+        render action: :edit
+      end
   end
 
   def destroy
       book = Book.find(params[:id])
       book.destroy
+      flash[:notice] = 'Book was successfully destroyed.'
       redirect_to books_path
   end
 
